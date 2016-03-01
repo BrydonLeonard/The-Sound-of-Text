b0a4cc90ff86825e21fe3bcf18ea1004623f2a16
Brydon Leonard <brydon.leonard@gmail.com>
2016-03-01 20:26:48 +0200

    Added major and minor players

--- src/TextToNotes/MidiManager.java
+++ src/TextToNotes/MidiManager.java
@@ -4,6 +4,7 @@ import javax.sound.midi.MidiChannel;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Synthesizer;
+import java.util.HashMap;
 
 /**
  * Created by biGb on 3/1/2016.
@@ -14,9 +15,45 @@ public class MidiManager {
     private int defaultLengthOff;
     private MidiChannel[] mc;
     private Synthesizer synth;
+    private HashMap<Integer, Integer> majorNotes;
+    private HashMap<Integer, Integer> minorNotes;
 
     /**
-     * Constructor taking parameters to set properties of the midi manager
+     * Initialises the synth and MidiChannels
+     * @throws MidiUnavailableException
+     */
+    private void initSynth() throws MidiUnavailableException {
+        synth = MidiSystem.getSynthesizer();
+        synth.open();
+        mc = synth.getChannels();
+    }
+
+    /**
+     * Populates a hashmap that determintes where the nth note is, relative to the 1st note.
+     */
+    private void setupNotes(){
+        majorNotes = new HashMap<>();
+        minorNotes = new HashMap<>();
+        int notes = 1;
+        majorNotes.put(0,0);
+        minorNotes.put(0,0);
+        while (notes < 100){
+            Integer degree = notes % 7;
+            Integer octave = Math.floorDiv(notes, 7);
+            if (degree.equals(3) || degree.equals(0))
+                majorNotes.put(notes, majorNotes.get(notes-1) + 1);
+            else majorNotes.put(notes, majorNotes.get(notes-1) + 2);
+            if (degree.equals(2) || degree.equals(5) || degree.equals(0))
+                minorNotes.put(notes, minorNotes.get(notes-1) + 1);
+            else if (degree.equals(5))
+                minorNotes.put(notes, minorNotes.get(notes-1) + 3);
+            else minorNotes.put(notes, minorNotes.get(notes-1) + 2);
+            notes++;
+        }
+    }
+
+    /**
+     * Constructor taking parameters to set properties of the midi manager. Also opens the synth, which must be closed once it is no longer being used.
      * @param defaultIntensity Default intensity at which notes are played
      * @param defaultLengthOn Default length that notes are played for
      * @param defaultLengthOff Default time between notes
@@ -26,24 +63,27 @@ public class MidiManager {
         this.defaultIntensity = defaultIntensity;
         this.defaultLengthOn = defaultLengthOn;
         this.defaultLengthOff = defaultLengthOff;
-        synth = MidiSystem.getSynthesizer();
-        synth.open();
-        mc = synth.getChannels();
-    }
+        this.initSynth();
+        this.setupNotes();
+}
 
     /**
-     * Constructor to set default parameters to preset values
+     * Constructor to set default parameters to preset values. Also opens the synth, which must be closed once it is no longer being used.
      * @throws MidiUnavailableException Thrown when MidiSystem is unavailable
      */
     public MidiManager() throws MidiUnavailableException {
         this.defaultIntensity = 60;
         this.defaultLengthOn = 200;
         this.defaultLengthOff = 200;
-        synth = MidiSystem.getSynthesizer();
-        synth.open();
-        mc = synth.getChannels();
+        initSynth();
+        this.setupNotes();
     }
 
+    /**
+     * An iterator for the notes of an ascending major scale (1 octave)
+     * @param start Starting note
+     * @param method A method to run on each note value
+     */
     public void forMajorUp(int start, Interator method){
         method.run(start);
         method.run(start+2);
@@ -55,6 +95,11 @@ public class MidiManager {
         method.run(start+12);
     }
 
+    /**
+     * An iterator for the notes of a descending major scale (1 octave)
+     * @param start Starting note
+     * @param method A method to run on each note value
+     */
     public void forMajorDown(int start, Interator method){
         method.run(start);
         method.run(start-1);
@@ -66,11 +111,21 @@ public class MidiManager {
         method.run(start-12);
     }
 
+    /**
+     * An iterator for the notes of an ascending and descending major scale, with a repeating top note
+     * @param start Starting note
+     * @param method A method to run on each note value
+     */
     public void forMajorUpDown(int start, Interator method){
         this.forMajorUp(start, method);
         this.forMajorDown(start+12, method);
     }
 
+    /**
+     * An iterator for the notes of an ascending harmonic minor scale
+     * @param start Starting note
+     * @param method A method to run on each note value
+     */
     public void forMinorUp(int start, Interator method){
         method.run(start);
         method.run(start+2);
@@ -82,6 +137,11 @@ public class MidiManager {
         method.run(start+12);
     }
 
+    /**
+     * An iterator for the notes of a descending harmonic minor scale
+     * @param start Starting note
+     * @param method A method to run on each note value
+     */
     public void forMinorDown(int start, Interator method){
         method.run(start);
         method.run(start-1);
@@ -93,11 +153,20 @@ public class MidiManager {
         method.run(start-12);
     }
 
+    /**
+     * An iterator for the notes of an ascending and descending harmonic minor scale
+     * @param start Starting note
+     * @param method A method to run on each note value
+     */
     public void forMinorUpDown(int start, Interator method){
         this.forMinorUp(start, method);
         this.forMinorDown(start+12, method);
     }
 
+    /**
+     * Plays a note with the corresponding midi pitch to i for the default length
+     * @param i Pitch value
+     */
     public void play(int i){
         try{
             mc[0].noteOn(i, defaultIntensity);
@@ -109,6 +178,37 @@ public class MidiManager {
         }
     }
 
+    /**
+     * Takes a string and plays it, using a major scale as a base
+     * @param notes The string
+     */
+    public void playMajor(String notes){
+        int[] n = fixStringToChar(notes);
+        for (int i : n){
+            this.play(majorNotes.get(i)+41);
+        }
+    }
+
+    public void playMinor(String notes){
+        int[] n = fixStringToChar(notes);
+        for (int i : n){
+            this.play(minorNotes.get(i) + 41);
+        }
+    }
+
+    private int[] fixStringToChar(String notes){
+         char[] c = notes.toLowerCase()
+                .replaceAll("[^a-z]", "")
+                 .toCharArray();
+        int[] n = new int[c.length];
+        for (int i = 0; i < c.length; i++)
+            n[i] = ((int)c[i])-97;
+        return n;
+    }
+
+    /**
+     * Closes the MidiManager
+     */
     public void close(){
         synth.close();
     }
--- src/TextToNotes/TextToNotesTest.java
+++ src/TextToNotes/TextToNotesTest.java
@@ -1,15 +1,22 @@
 package TextToNotes;
 
 import javax.sound.midi.*;
-import javax.sound.sampled.AudioPermission;
+import java.util.Scanner;
 
 /**
  * Created by biGb on 3/1/2016.
  */
 public class TextToNotesTest {
     public static void main(String[] args) throws InvalidMidiDataException, MidiUnavailableException, InterruptedException {
-        MidiManager mm = new MidiManager();
-        mm.forMajorUpDown(50, (i) -> mm.play(i));
-        mm.forMinorUpDown(50, (i) -> mm.play(i));
+        MidiManager mm = new MidiManager(60, 150, 20);
+        System.out.println("Enter your string");
+        Scanner scnr = new Scanner(System.in);
+        String str = scnr.nextLine();
+
+        mm.playMajor(str);
+        Thread.sleep(1000);
+        mm.playMinor(str);
+        Thread.sleep(1000);
+        mm.close();
     }
 }
